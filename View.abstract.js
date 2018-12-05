922e1e01a329ecc6df8b8d0469866e38b77bd0d9
Meathill <meathill@gmail.com>
2018-12-05 13:00:36 +0800

    change: added some new feature.

--- View.abstract.js
+++ View.abstract.js
@@ -1,10 +1,39 @@
-export default class View {
-  constructor(el) {
-    this.el = el;
+import EventEmitter from 'eventemitter3';
+
+export default class View extends EventEmitter {
+  constructor(el, queue, options) {
+    super();
+    this.queue = queue;
+    this.el = el || this.createElement(options);
     this.delegateEvents();
   }
 
-  delegateEvents() {
+  createElement(options) {
+    const el = document.createElement('div');
+    const {id, className} = options;
+    if (id) {
+      el.id = id;
+    }
+    if (className) {
+      el.className = className;
+    }
+    return el;
+  }
 
+  delegateEvents() {
+    this.el.addEventListener('animationend', event => {
+      if (event.target === this.el && event.animationName === 'fadeOut') {
+        this.el.classList.add('hide');
+      }
+    });
+  }
+
+  fadeOut() {
+    this.el.classList.add('animated', 'fadeOut');
+  }
+
+  getAssetUrl(asset) {
+    asset = this.queue.getResult(asset, true);
+    return URL.createObjectURL(asset);
   }
 }
--- wukong.js
+++ wukong.js
@@ -7,6 +7,12 @@ export default class Wukong {
   constructor({loading, progress}) {
     this.loading = loading;
     this.progress = document.querySelector(progress);
+    this.callbacks = {};
+
+    this.onProgress = this._onProgress.bind(this);
+    this.onComplete = this._onComplete.bind(this);
+    this.onFileLoaded = this._onFileLoaded.bind(this);
+
     if ('Promise' in window && window['Promise'] instanceof Function) {
       this.loadCreateJS();
     } else {
@@ -22,19 +28,20 @@ export default class Wukong {
     load(cdn.createjs)
       .then(() => {
         let queue = this.queue = new createjs.LoadQueue(true, BASE_PATH);
-        queue.on('progress', this.onProgress.bind(this));
-        queue.on('complete', this.onComplete.bind(this));
-        for (let i = 0, len = this.assets.length; i < len; i++) {
-          if (this.assets[i] instanceof Array) {
-            this.queue.loadManifest(this.assets[i]);
+        queue.on('progress', this.onProgress);
+        queue.on('complete', this.onComplete);
+        queue.on('fileload', this.onFileLoaded);
+        this.assets.forEach(item => {
+          if (item instanceof Array) {
+            this.queue.loadManifest(item);
           } else {
-            this.queue.loadFile(this.assets[i]);
+            this.queue.loadFile(item);
           }
-        }
+        });
       });
   }
 
-  onComplete() {
+  _onComplete() {
     console.log('Wukong: all loaded');
     let app = new H5App(this.queue);
 
@@ -43,7 +50,12 @@ export default class Wukong {
     }
   }
 
-  onProgress(event) {
+  _onFileLoaded(event) {
+    const {item} = event;
+    item && item.onload && item.onload();
+  }
+
+  _onProgress(event) {
     console.log(event.progress);
     let progress = event.progress * 100 >> 0;
     this.progress.innerText = `${progress}%`;
